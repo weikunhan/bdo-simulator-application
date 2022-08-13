c86d27874521bcbe33199d29ab7c9dc380c96046
Weikun Han <weikunhan@g.ucla.edu>
2022-08-12 20:43:25 -0700

    fix format

--- bdo_simulator_app.py
+++ bdo_simulator_app.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 # MIT License
-# 
+#
 # Copyright (c) 2022 Weikun Han
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,12 +27,12 @@ Reference:
 """
 
 import time
-import numpy as np
-import streamlit as st 
-import streamlit.components.v1 as stc 
 from datetime import datetime
+import numpy as np
+import streamlit as st
+import streamlit.components.v1 as stc
 
-custom_title = '''
+CUSTOM_TITLE = '''
 <div style="font-size:40px;font-weight:bolder;background-color:#fff;padding:10px;
 border-radius:10px;border:5px solid #464e5f;text-align:center;">
 		<span style='color:black'>B</span>
@@ -58,21 +58,21 @@ border-radius:10px;border:5px solid #464e5f;text-align:center;">
 # pylint: disable=invalid-name
 def simulate_bdo_succeeded_rate(
     succeeded_rate: float,
-    simiulated_times = 1000,
+    simiulated_times = 10000,
     time_range: int = 600,
-    time_buffer: int = 0, 
+    time_buffer: int = 0,
     time_utc_sec: int = None
 ) -> tuple:
     '''Predict game random generator bias for UTC time and succeeded rate
 
-    Game used C++ implemetation. 
+    Game used C++ implemetation.
 
     https://cplusplus.com/reference/cstdlib/rand/
     rand() is possible funntion to call, but is sometimes rather poor quality.
 
     https://en.cppreference.com/w/cpp/numeric/random
     Mersenne twister algorithm is better for random number generator
-    
+
     https://stackoverflow.com/questions/9523570/random-number-generation-with-c-or-python
     The range for python set to [0, RAND_MAX].
     RAND_MAX is 32767 = 2^15-1, so the maximum value of a 16-bit signed integer.
@@ -103,7 +103,7 @@ def simulate_bdo_succeeded_rate(
 
     #print(f'succeeded_rate={succeeded_rate}, time_utc_in_sec={time_utc_now_sec},'
     #      f' time_range={time_range}, time_buffer={time_buffer},'
-    #      f' simiulated_times={simiulated_times}')    
+    #      f' simiulated_times={simiulated_times}')
 
     for i in range(time_range):
         positive_case_count = 0
@@ -117,7 +117,7 @@ def simulate_bdo_succeeded_rate(
             random_value = n % 10000
             #max_value = max(max_value, random_value)
             #min_value = min(min_value, random_value)
-            
+
             # succeeded if random value is less or equal to the succeeded rate
             if random_value <= succeeded_rate * 100:
                 positive_case_count += 1
@@ -127,7 +127,7 @@ def simulate_bdo_succeeded_rate(
                 negative_case_count += 1
 
         #print(f'time={random_seed_value}, positive_case_count={positive_case_count},'
-        #      f'negative_case_count={negative_case_count}')        
+        #      f'negative_case_count={negative_case_count}')
         avg_succeeded_count = (positive_case_count + negative_case_count) / 2.0
 
         if avg_succeeded_count > best_succeeded_count:
@@ -138,9 +138,11 @@ def simulate_bdo_succeeded_rate(
     return best_time_utc_sec, best_succeeded_rate
 
 def main():
+    ''' Main funtion'''
+
     #st.title('Black Desert Online simulator ')
-    stc.html(custom_title)
-    menu = ['Black Desert Online simulator v1', 
+    stc.html(CUSTOM_TITLE)
+    menu = ['Black Desert Online simulator v1',
             'Black Desert Online simulator v2',
             'Black Desert Online simulator v3',
             'About']
@@ -154,11 +156,11 @@ def main():
             max_value=100.00,
             format='%.2f')
         st.info(f'The input succeeded rate: {succeeded_rate}%', icon="ℹ️")
-        st.text(f'The current config setting for simulator')
-        st.text(f' - simulated_times (total simulation run each second): 1000')
-        st.text(f' - time_range (future time window in seconds): 600')
-        st.text(f' - time_buffer (possible server latch in seconds): 0')
-        st.text(f' - time_utc_sec (current UTC time): '
+        st.text('The current config setting for simulator')
+        st.text(' - simulated_times (total simulation run each second): 10000')
+        st.text(' - time_range (future time window in seconds): 600')
+        st.text(' - time_buffer (possible server latch in seconds): 0')
+        st.text(' - time_utc_sec (current UTC time): '
                 f'{datetime.utcnow().strftime("%A, %B %d, %Y %I:%M:%S")}')
 
         if succeeded_rate:
@@ -178,13 +180,13 @@ def main():
                 time.sleep(1)
     elif choice == 'Black Desert Online simulator v2':
         st.subheader('Black Desert Online simulator v2')
-        st.info(f'This is info')
-        st.text(f'This is text')
+        st.info('This is info')
+        st.text('This is text')
         st.write(f'This is {"write"}')
     elif choice == 'Black Desert Online simulator v3':
         st.subheader('Black Desert Online simulator v3')
-        st.info(f'This is info')
-        st.text(f'This is text')
+        st.info('This is info')
+        st.text('This is text')
         st.write(f'This is {"write"}')
     else:
         st.subheader('About')
